f0926c2ed0206a8bbc2eb61c19a77c1ed92a3086
Aditya Raj <adityaraj34@icloud.com>
2026-02-16 15:06:02 +0530

    gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import {useGSAP} from "@gsap/react"
 
 const App = () => {
+//todo implement the gsap.to() methdo
+useGSAP(()=>{
+  gsap.to("#blue-box",{
+    x:250,
+    repeat:-1,
+    yoyo:true,
+    rotation:360,
+  })
+
+},[])
+
   return (
-    <div>
-      
+    <div className='bg-blue-500  h-11 w-3xl '>
+      hello world
     </div>
   )
 }
