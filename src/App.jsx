c40edec0c57ca49d5d4e552826cb6df16ad7a4a9
Aditya Raj <adityaraj34@icloud.com>
2026-02-17 12:44:14 +0530

    all done

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import {useGSAP} from "@gsap/react"
+import React from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const App = () => {
-//todo implement the gsap.to() methdo
-useGSAP(()=>{
-  gsap.to("#blue-box",{
-    x:250,
-    repeat:-1,
-    yoyo:true,
-    rotation:360,
-  })
 
-},[])
+  useGSAP(() => {
+    gsap.to("#blue-box", {
+      x: 250,
+      repeat: -1,
+      yoyo: true,
+      rotation: 360,
+      duration: 2,
+      ease: "elastic.out(1, 0.3)"
+    });
+  }, []);
 
   return (
-    <div className='bg-blue-500  h-11 w-3xl '>
-      hello world
+    <div className="h-screen flex items-center justify-center bg-black">
+      <div
+        id="blue-box"
+        className="w-40 h-40 bg-blue-500"
+      ></div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
