b0b8c245a1877e3ed2a8156e328ffa85f2263d12
Aditya Raj <adityaraj34@icloud.com>
2026-02-17 14:41:24 +0530

    all done

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-import React from "react";
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
+import React from 'react'
 
-const App = () => {
-
-  useGSAP(() => {
-    gsap.to("#blue-box", {
-      x: 250,
-      repeat: -1,
-      yoyo: true,
-      rotation: 360,
-      duration: 2,
-      ease: "elastic.out(1, 0.3)"
-    });
-  }, []);
-
+function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-black">
-      <div
-        id="blue-box"
-        className="w-40 h-40 bg-blue-500"
-      ></div>
+    <div>
+      
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
